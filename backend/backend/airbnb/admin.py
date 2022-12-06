8b9bff5a72fd070ccbdba2304fecd95be9fd13e5
jay <jieyuan1@ualberta.ca>
2022-12-05 16:25:47 -0800

    added some backend logics

--- backend/backend/airbnb/admin.py
+++ backend/backend/airbnb/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(User)
+admin.site.register(Avg_Price_License)
+admin.site.register(Corela_Rooms_Beds_Accommodates)
+admin.site.register(Price_NumberOfBedRoom)
